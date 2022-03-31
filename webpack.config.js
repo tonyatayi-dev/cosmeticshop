8445374751e518567cb1f78ec2ab33780c93ef32
tonyatayi <aatayi@outlook.com>
2022-03-31 17:48:59 +0100

    final

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+const postCSSPlugins = [require("postcss-simple-vars"), require("postcss-nested"), require("autoprefixer")]
+
+module.exports = {
+    entry: './js/App.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, '')
+    },
+    mode: "development",
+    watch: true,
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader?url=false", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
+        }
+      ]
+    }
+  }
